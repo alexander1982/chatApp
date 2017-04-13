8f69e70a47b3d06e33a21802d9bf51d07274dceb
Alex Gorelikov <gorelikov.alex@gmail.com>
2017-04-13 13:59:22 +0300

    Geolocation via geo API by google

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 var socket = io();
 socket.on('connect', function() {
 	console.log('Connected to server');
@@ -9,5 +10,47 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', (message) => {
 	console.log('Got new message', message);
+	var li = $('<li></li>');
+	li.text(`${message.from}: ${message.text}`);
+	
+	$('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message)  {
+	var li = $('<li></li>');
+	var a = $('<a target="_blank">My current location</a>');
+	
+	li.text(`${message.from}: `);
+	a.attr('href', message.url);
+	li.append(a);
+	$('#messages').append(li);
+});
+
+$('#message-form').on('submit', function(e) {
+	e.preventDefault();
+	socket.emit('createMessage', {
+		from: 'User',
+		text: $('[name=message]').val()
+	}, function() {
+		
+	});
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click', function(e) {
+	e.preventDefault();
+	if(!navigator.geolocation){
+		return alert('Geolocation not supported by your browser');
+	}
+	
+	navigator.geolocation.getCurrentPosition(function(position) {
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		});
+		
+		console.log(position);
+	}, function() {
+		alert('Unable to fetch the location');
+	})
+});
